998830c0005ab0d623ebc7c5c0a7e17cca76fc8d
GovindJeeParihar <govindam88@gmail.com>
2020-10-23 17:14:15 +0530

    Updates

---
+++ precache-manifest.baf507e90507af7ca568b2cd6937c89d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df6c216e3aa3de047a90d565d15a888",
+    "url": "/Akaro/index.html"
+  },
+  {
+    "revision": "ece6c07a67e3c99382c2",
+    "url": "/Akaro/static/css/main.16ada8d2.chunk.css"
+  },
+  {
+    "revision": "b7df90083991dbd7076c",
+    "url": "/Akaro/static/js/2.750488e1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Akaro/static/js/2.750488e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece6c07a67e3c99382c2",
+    "url": "/Akaro/static/js/main.764ad8b4.chunk.js"
+  },
+  {
+    "revision": "ffd091c44f4ae890470c",
+    "url": "/Akaro/static/js/runtime-main.58f5c572.js"
+  },
+  {
+    "revision": "1637fe2ec63875d61fae49391ce0ad65",
+    "url": "/Akaro/static/media/Slider1.1637fe2e.jpg"
+  },
+  {
+    "revision": "ed9ce907b54c2e69f421b8d0778f8c03",
+    "url": "/Akaro/static/media/about-bg.ed9ce907.jpg"
+  },
+  {
+    "revision": "35434661f9850954ac6e61812f94a914",
+    "url": "/Akaro/static/media/contact-bg.35434661.jpg"
+  },
+  {
+    "revision": "a2d2c849cf448dfe14e4f4f56fd9efeb",
+    "url": "/Akaro/static/media/foot-bg.a2d2c849.jpg"
+  },
+  {
+    "revision": "0a6990085cb8a82bcf5d8a63578eb254",
+    "url": "/Akaro/static/media/welcome-bg.0a699008.jpg"
+  },
+  {
+    "revision": "a1834b2ed41dbf7e3d71ebecc2671372",
+    "url": "/Akaro/static/media/welcome.a1834b2e.jpg"
+  }
+]);
